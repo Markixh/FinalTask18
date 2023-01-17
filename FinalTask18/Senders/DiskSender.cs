0cb8759666ad82211a0df70423408cd9d9fee4e0
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-01-17 20:01:23 +0300

    Добавлен класс отправителя команды, диск

---
+++ FinalTask18/Senders/DiskSender.cs
@@ -0,0 +1,19 @@
+﻿using FinalTask18.Commands;
+
+namespace FinalTask18.Senders
+{
+    internal class DiskSender : ISender
+    {
+        ICommand command;
+
+        public DiskSender(ICommand command) 
+        {
+            this.command = command;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Команда отправлена");
+        }
+    }
+}
